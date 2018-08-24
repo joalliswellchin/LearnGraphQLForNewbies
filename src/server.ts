408b3bbbb408b7fa149754e38a2057ef1a3a9313
joel <joel@cynopsis.co>
2018-08-24 17:59:50 +0800

    set ts file to src

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { buildSchema, graphql } from "graphql";
+
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+const root = {
+   hello: () => {
+      return "Hello world!";
+   },
+};
+
+graphql(schema, "{hello}", root).then((response) => {
+   console.log(response);
+});
